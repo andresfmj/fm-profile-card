abd1e4c5c178056fdcb127300c14cf84e8bc736b
Andres Fernando <andres.fernando08@gmail.com>
2021-01-02 16:07:54 -0500

    [add] background image and responsive media query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import ProfileCard from '../containers/ProfileCard';
 
 import '../styles/containers/app.scss';
 
+import bgTop from '../images/bg-pattern-top.svg';
+import bgBottom from '../images/bg-pattern-bottom.svg';
+
 
 function App() 
 {
   return (
-    <div className="App">
-      <ProfileCard />
-    </div>
+    <Fragment>
+      <div className="bgTop" style={styles.backgroundTopStyle}></div>
+      <div className="bgBottom" style={styles.backgroundBottomStyle}></div>
+      <div className="App">
+        <ProfileCard />
+      </div>
+    </Fragment>
   )
+}
+
+const styles = {
+  backgroundTopStyle: {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    backgroundImage: `url(${bgTop})`
+  },
+  backgroundBottomStyle: {
+    position: 'absolute',
+    bottom: '0',
+    right: '0',
+    backgroundImage: `url(${bgBottom})`
+  }
 }
 
 export default App;
