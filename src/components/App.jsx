03d9fd9a2386582ee81745152f5a5df71534197a
Andres Fernando <andres.fernando08@gmail.com>
2021-01-02 11:54:53 -0500

    [add] initial react files

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ProfileCard from '../containers/ProfileCard';
+
+
+function App() 
+{
+  return (
+    <ProfileCard />
+  )
+}
+
+export default App;
---
+++ src/containers/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProfileCard()
+{
+  return (
+    <div className="ProfileCard">
+      ProfileCardContainer
+    </div>
+  )
+}
+
+export default ProfileCard;
